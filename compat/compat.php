9b07f517d3926b5bc77c3368cfecf52b94d0d890
Alessandro Chitolina <alekitto@gmail.com>
2023-01-02 11:41:16 +0100

    chore: remove dbal 2 compat layer

--- compat/compat.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-require_once __DIR__.'/Common/Persistence/Event/LifecycleEventArgs.php';
-require_once __DIR__.'/Common/Persistence/Mapping/Driver/MappingDriver.php';
-require_once __DIR__.'/Common/Persistence/Mapping/ClassMetadata.php';
-require_once __DIR__.'/Common/Persistence/Mapping/ClassMetadataFactory.php';
-require_once __DIR__.'/Common/Persistence/Mapping/MappingException.php';
-require_once __DIR__.'/Common/Persistence/Mapping/RuntimeReflectionService.php';
-require_once __DIR__.'/Common/Persistence/ObjectRepository.php';
